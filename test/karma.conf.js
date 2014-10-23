3543e67969b242191a760133f6f7a80d17314f08
Ariel Mashraki <ariel@mashraki.co.il>
2014-10-23 21:50:09 +0300

    test(config): jahint, karma, etc

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -29,8 +29,6 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-mocks/angular-mocks.js',
       'src/**/*.js',
       'src/**/**/*.js',
       'test/spec/**/*.js',
---
+++ test/spec/parse.js
@@ -0,0 +1,7 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                     //
+// $parse                                                                              //
+// this unit should nor be tested again, cuz it's actually tested by the angular team, //
+// see: github.com/angular.js/angular.js                                               //
+//                                                                                     //
+/////////////////////////////////////////////////////////////////////////////////////////
