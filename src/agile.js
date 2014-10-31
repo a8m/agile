ebdfb082d77628c8f2ece799c655fcb42e83c47c
Ariel Mashraki <ariel@mashraki.co.il>
2014-10-31 15:25:21 +0200

    feat(agile): StringWrapper, ArrayWrapper

--- src/agile.js
+++ src/agile.js
@@ -1,8 +1,63 @@
 /**
- *
+ * @const
+ * prototype methods, used for chaining and static methods
+ */
+var PROTO_METHODS = {
+  STRING: ['charAt', 'concat', 'indexOf', 'lastIndexOf', 'match', 'replace','slice', 'substr', 'substring', 'toLowerCase', 'toUpperCase'],
+  ARRAY:  ['concat', 'join', 'pop', 'push', 'shift', 'sort', 'splice', 'unshift']
+};
+
+var AGILE_METHODS = {
+  STRING: [startsWith, endsWith, trim, ltrim, rtrim, repeat, slugify, stringular, stripTags, truncate, ucfirst, wrap],
+  ARRAY:  [after, afterWhere, before, beforeWhere, contains, countBy, defaults, map, contains, first,
+          filter, last, flatten, groupBy, omit, filter, remove, reverse, unique, xor, max, min, sum]
+};
+
+/**
+ * StringWrapper
+ * @param value
+ * @constructor
+ */
+function StringWrapper(value) {
+  this.__value__ = value;
+}
+//bind the methods to StringWrapper.prototype and agile
+forEach(PROTO_METHODS.STRING, function(methodName) {
+  StringWrapper.prototype[methodName] = function() {
+    var func = String.prototype[methodName];
+    var res  = func.apply(this.__value__, arguments);
+    return isString(res)
+      ? new StringWrapper(res)
+      : new ArrayWrapper(res)
+  }
+});
+
+/**
+ * ArrayWrapper
+ * @param value
+ * @constructor
+ */
+function ArrayWrapper(value) {
+  this.__value__ = value;
+}
+//bind the methods to StringWrapper.prototype and agile
+forEach(PROTO_METHODS.ARRAY, function(methodName) {
+  ArrayWrapper.prototype[methodName] = function() {
+    var func = Array.prototype[methodName];
+    var res  = func.apply(this.__value__, arguments);
+    return isString(res)
+      ? new StringWrapper(res)
+      : new ArrayWrapper(res)
+  }
+});
+
+
+/**
+ * @constructor agile
  * @param value
  */
 function agile(value) {
+  return new ArrayWrapper(value);
 }
 
 // @static boolean methods
@@ -25,6 +80,7 @@ agile.noop      = noop;
 agile.uppercase = uppercase;
 agile.lowercase = lowercase;
 agile.toJson    = toJson;
+agile.forEach   = forEach;
 
 //@static array methods
 agile.after       = after;
