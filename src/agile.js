a0adb601c0bb19fcac70365ebcba2cdd93cbaf35
Ariel Mashraki <ariel@mashraki.co.il>
2014-11-01 14:45:21 +0200

    feat(NumberWrapper): create

--- src/agile.js
+++ src/agile.js
@@ -12,11 +12,13 @@ var UNWRAPPED_FUNC = /^(?:value|identity)$/;
  */
 var PROTO_METHODS = {
   STRING: ['charAt', 'concat', 'indexOf', 'lastIndexOf', 'match', 'replace','slice', 'substr', 'substring', 'toLowerCase', 'toUpperCase'],
-  ARRAY:  ['concat', 'join', 'pop', 'push', 'shift', 'sort', 'splice', 'unshift']
+  ARRAY:  ['concat', 'join', 'pop', 'push', 'shift', 'sort', 'splice', 'unshift'],
+  NUMBER: ['abs', 'ceil', 'cos', 'floor', 'round', 'sin', 'sqrt', 'pow', 'tan']
 };
 
 var AGILE_METHODS = {
   BASE  : [value],
+  OBJECT: [{ name: 'keys', action: objKeys }, toArray],
   STRING: [startsWith, endsWith, trim, ltrim, rtrim, repeat, slugify, stringular, stripTags, truncate, ucfirst, wrap],
   ARRAY : [after, afterWhere, before, beforeWhere, contains, countBy, defaults, map, contains, first,
           filter, last, flatten, groupBy, omit, filter, remove, reverse, unique, xor, max, min, sum]
@@ -34,10 +36,14 @@ var AGILE_METHODS = {
 function defineWrapperPrototype(ctor, methods, prototype) {
   forEach(methods, function(method) {
     var methodName = isString(method) ? method : method.name;
-    var func = isString(method) ? prototype[method] : method;
+    var func = isString(method) ? prototype[method]
+      : isObject(method) ? method.action //if it's method with custom name
+      : method;
     ctor.prototype[methodName] = function() {
       var args = [this.__value__].concat(Array.prototype.slice.call(arguments));
-      var res  = isString(method)
+      //if it's a prototype function, but not a static function, e.g: Math.pow
+      //I'm not proud of this, but it'll get the job done for now.
+      var res  = isString(method) && !(prototype[methodName])
         ? func.call(this.__value__, arguments)
         : func.apply(this, args);
       return UNWRAPPED_FUNC.test(methodName)
@@ -82,17 +88,68 @@ defineWrapperPrototype(StringWrapper, stringWrapperMethods, String.prototype);
 function ArrayWrapper(value) {
   this.__value__ = value;
 }
-//bind the collection methods to ArrayWrapper.prototype and agile
+//bind the collection methods to ArrayWrapper.prototype
 var arrayWrapperMethods = flatten([PROTO_METHODS.ARRAY, AGILE_METHODS.ARRAY, AGILE_METHODS.BASE]);
 defineWrapperPrototype(ArrayWrapper, arrayWrapperMethods, Array.prototype);
 
+/**
+ * @constructor ObjectWrapper
+ * @description
+ * wraps an object and implements the methods of object and Object.prototype
+ * @param value {Object}
+ */
+function ObjectWrapper(value) {
+  this.__value__ = value;
+}
+//bind the object methods to ObjectWrapper.prototype
+var objectWrapperMethods = flatten([AGILE_METHODS.OBJECT, AGILE_METHODS.BASE]);
+defineWrapperPrototype(ObjectWrapper, objectWrapperMethods, Object.prototype);
+
+/**
+ * @constructor NumberWrapper
+ * @description
+ * wraps an number and implements the methods of number and Number.prototype
+ * @param value {Number}
+ */
+function NumberWrapper(value) {
+  this.__value__ = value;
+}
+//bind the number methods to NumberWrapper.prototype
+var numberWrapperMethods = flatten([PROTO_METHODS.NUMBER, AGILE_METHODS.BASE]);
+defineWrapperPrototype(NumberWrapper, numberWrapperMethods, Math);
+
+/**
+ * @private
+ * @description
+ * return Wrapper::constructor based on given value
+ * @param val
+ * @returns {*}
+ */
+function getWrapperCtor(val) {
+  switch (typeof val) {
+    case 'string':
+      return StringWrapper;
+    case 'number':
+      return NumberWrapper;
+    case 'object':
+      return isArray(val) ? ArrayWrapper : ObjectWrapper;
+    default :
+      throw new Error('Agile value can\'t get ['+ typeof val + '] as an argument');
+  }
+}
 
 /**
  * @constructor agile
  * @param value
  */
 function agile(value) {
-  return new ArrayWrapper(value);
+  if(value.__wrapped__) {
+    return value;
+  } else {
+    var ctor = getWrapperCtor(value), inst;
+    (inst = new ctor(value)).__wrapped__ = true;
+    return inst;
+  }
 }
 //@static methods as wrappers
 var agileStaticMethods = flatten([AGILE_METHODS.ARRAY, AGILE_METHODS.STRING]);
