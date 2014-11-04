95439b79b90aac5579174056f3781212b4af1790
pavelkerbel <pavel.kerbel@ironsrc.com>
2014-11-04 20:16:07 +0200

    feat(max): issue: #4

--- src/_agile/array/max.js
+++ src/_agile/array/max.js
@@ -3,10 +3,14 @@
  * @kind function
  *
  * @description
- * Math.max
+ * Math.max will get an array return the max value. if an expression
+ * is provided, will return max value by expression.
  */
-function max(input) {
-    return (isArray(input)) ?
-        Math.max.apply(Math, input) :
-        input;
+function max(input, expression) {
+    if(!isArray(input)) {
+        return input;
+    }
+    return isUndefined(expression)
+        ? Math.max.apply(Math, input)
+        : input[indexByMath('max', input, expression)]
 }
