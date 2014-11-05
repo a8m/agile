1699b7d036ffccbc560f4c47e66a1ef8616fa732
Ariel Mashraki <ariel.mashraki@ironsrc.com>
2014-11-05 13:36:18 +0200

    doc(add): add docs to add function

--- src/_agile/chain.js
+++ src/_agile/chain.js
@@ -1,4 +1,15 @@
-//these methods is kind of common for chaining wrapper
+//these methods is kind of common methods for chaining wrappers
+/**
+ * @description
+ * add methods get an object extend(based on type) and return it.
+ * @param object
+ * @returns {*}
+ * @example
+ * add(1,2) ==> 3
+ * add([],1) ==> [1]  
+ * add('f','g') ==> 'fg'
+ * add({}, {a:1}) ==> {a:1}
+ */
 function add(object) {
   var args = Array.prototype.slice.call(arguments, 1);
   //loop through all over the arguments
