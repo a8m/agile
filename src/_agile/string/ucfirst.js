093049fdf1cc0558c69ed61e0fc1882df6a67b08
Ariel Mashraki <ariel@mashraki.co.il>
2015-02-06 22:04:28 +0200

    style(ucfirst)

--- src/_agile/string/ucfirst.js
+++ src/_agile/string/ucfirst.js
@@ -6,8 +6,10 @@
  * upper case first char
  */
 function ucfirst(input) {
-  return isString(input) ? input.split(' ')
-    .map(function (char) {
-      return char.charAt(0).toUpperCase() + char.substring(1);
-    }).join(' ') : input;
+  return isString(input) 
+    ? input.split(' ')
+      .map(function (char) {
+        return char.charAt(0).toUpperCase() + char.substring(1);
+      }).join(' ') 
+    : input;
 }
